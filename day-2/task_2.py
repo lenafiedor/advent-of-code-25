dd4c9f60ba08b77956481a621a90501806968f24
Lena Fiedor <lenafiedor@o2.pl>
2025-12-04 11:35:51 +0200

    day 2 complete

---
+++ day-2/task_2.py
@@ -0,0 +1,23 @@
+import re
+
+FILE_NAME = 'input.txt'
+
+def find_patterns(rng):
+    sum = 0
+    for num in range(rng[0], rng[1] + 1):
+        pattern = r'^(\d+)\1+$'
+        result = re.match(pattern, str(num))
+        if result:
+            sum += num
+
+    return sum
+
+
+with open(FILE_NAME) as f:
+    ranges = f.read().strip().split(',')
+    ranges_num = [list(map(int, r.split('-'))) for r in ranges]
+    sum = 0
+
+    for r in ranges_num:
+       sum += find_patterns(r)
+    print(f"Sum: {sum}")
